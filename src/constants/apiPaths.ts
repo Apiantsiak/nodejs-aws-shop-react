4ac8c216b4052611aba089433206756c831dace4
Aliaksei Piantsiak <avpentyak@gmail.com>
2023-11-27 12:23:20 +0100

    change products URL for BE integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://t686pmlbgc.execute-api.us-east-1.amazonaws.com",
+  product: "https://udzxvt247yb5vuxsobamm2rjmu0wbear.lambda-url.us-east-1.on.aws",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
