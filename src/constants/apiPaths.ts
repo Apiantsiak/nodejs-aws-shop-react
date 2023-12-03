fe0f75701e7b0ff74e3fb401252cffb36fffe1a4
Aliaksei Piantsiak <avpentyak@gmail.com>
2023-12-03 19:20:59 +0100

    Change import service url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
-  product: "https://udzxvt247yb5vuxsobamm2rjmu0wbear.lambda-url.us-east-1.on.aws",
+  product: " https://3t2yefwrygnnvy3v7drlxqvr7y0jpffr.lambda-url.us-east-1.on.aws",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://am747zkzc9.execute-api.us-east-1.amazonaws.com/dev/import",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
